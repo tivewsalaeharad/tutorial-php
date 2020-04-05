6a834cc37ec824d759534fbfd369bb347483ea39
tivewsalaeharad <tivew@mail.ru>
2020-04-05 12:07:29 +0300

    Corrections

--- app/views/error/unknownMethod.php
+++ app/views/error/unknownMethod.php
@@ -2,3 +2,8 @@
 echo "Метод {$data['method']} класса {$data['class']} не найден";
 echo '<br>';
 echo getcwd();
+echo '<br>';
+echo realpath('..');
+echo '<br>';
+echo realpath('../app');
+echo '<br>';
