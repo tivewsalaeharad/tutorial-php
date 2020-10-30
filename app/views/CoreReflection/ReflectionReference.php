2ccb511e876ecf83baa12bb5ce88b6b8043f75da
Pavel <tivew@mail.ru>
2020-10-31 01:43:23 +0300

    ReflectionReference

--- app/views/CoreReflection/ReflectionReference.php
+++ app/views/CoreReflection/ReflectionReference.php
@@ -1,4 +1,14 @@
 <?php
+$val1 = 'foo';
+$val2 = 'bar';
+$arr = [&$val1, &$val2, &$val1];
 
-$rr = ReflectionReference::fromArrayElement([25, 36, 49], 2);
-echo $rr->getId();
+$rr1 = ReflectionReference::fromArrayElement($arr, 0);
+$rr2 = ReflectionReference::fromArrayElement($arr, 1);
+$rr3 = ReflectionReference::fromArrayElement($arr, 2);
+
+var_dump($rr1->getId());
+var_dump($rr2->getId());
+var_dump($rr3->getId());
+var_dump($rr1->getId() === $rr2->getId());
+var_dump($rr1->getId() === $rr3->getId());
