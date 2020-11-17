942ab8676f2767044ea3135ae393bdbc2b63e7bb
Pavel <tivew@mail.ru>
2020-11-18 00:30:15 +0300

    system

--- app/views/CoreShell/shell_exec.php
+++ app/views/CoreShell/shell_exec.php
@@ -1,4 +1,8 @@
 <pre>
-<?=shell_exec('php -v')?>
-<?=htmlspecialchars(shell_exec('findstr /i /n /c:"datepicker" /d:"W:\domains\EOPMS\eopms";"W:\domains\EOPMS\eopms\js\ag-grid-tuning" "*.*"'))?>
+    <?=shell_exec('php -v')?>
+    <!--htmlspecialchars(shell_exec('findstr /i /n /c:"datepicker" /d:"W:\domains\EOPMS\eopms";"W:\domains\EOPMS\eopms\js\ag-grid-tuning" "*.*"'))-->
+    <?=shell_exec('ls -al')?>
+
+
+    <?=shell_exec('cd .. && ls -al')?>
 </pre>
