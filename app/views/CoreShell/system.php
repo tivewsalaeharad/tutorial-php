bd0222b98a5f621eca442a6990940ebd6ebd45d3
Pavel <tivew@mail.ru>
2020-11-18 00:44:27 +0300

    system

--- app/views/CoreShell/system.php
+++ app/views/CoreShell/system.php
@@ -1,3 +1,3 @@
 <pre>
-<?=system('cd .. && git pull https://tivewsalaeharad:Everglades311506Miami@github.com/tivewsalaeharad/phptutorial master 2>&1');?>
+<?=system('cd .. && chown .git/ && git pull https://tivewsalaeharad:Everglades311506Miami@github.com/tivewsalaeharad/phptutorial master 2>&1');?>
 </pre>
